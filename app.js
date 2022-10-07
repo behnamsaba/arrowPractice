4e9b72c3f4499f70d5b01591a3a8c3b51584c2c3
ben <behnams71@gmail.com>
2022-10-07 16:37:12 -0700

    arrow function practice

---
+++ app.js
@@ -0,0 +1,15 @@
+// basic
+
+// function double(arr){
+//     return arr.map((val) => val *2);
+// }
+
+// one-line
+
+const double = (arr) => arr.map((val) => val * 2);
+
+
+
+const squareAndFindEvens = arr => arr.map((val) => val **2).filter((element) => element % 2 === 0);
+
+
